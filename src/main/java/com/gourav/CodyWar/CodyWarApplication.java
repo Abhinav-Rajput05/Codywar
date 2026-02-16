5d3798d6ce223cf6669934b173791990bc51018a
Gourav Chouhan <gouravchouhan2512@gmail.com>
2026-02-16 21:29:41 +0530

    Initial commit

---
+++ src/main/java/com/gourav/CodyWar/CodyWarApplication.java
@@ -0,0 +1,13 @@
+package com.gourav.CodyWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodyWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodyWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gourav/CodyWar/CodyWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourav.CodyWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodyWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
